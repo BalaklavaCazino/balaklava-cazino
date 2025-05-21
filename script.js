b9257a5e6f8ff4968426d59bef805a3de96a94d2
BalaklavaCazino <mrmaksim12345@gmail.com>
2025-05-21 13:17:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+let balance = 5; // Начальный баланс с бонусом за регистрацию
+document.getElementById('balance').innerText = balance + '₴';
 
+function toggleLogin() {
+    // Здесь можно добавить логику для формы входа
+    alert('Форма входа будет добавлена позже.');
+}
